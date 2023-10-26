5e75eb62c1a80d472c5c728562e145b8e25969d1
X-62 <142080539+X-62@users.noreply.github.com>
2023-10-26 12:33:19 +0330

    Update X.sh

--- X.sh
+++ X.sh
@@ -1,3 +1,7 @@
+clear
+printf "X-62"
+printf "____________________"
 echo "To connect the proxy to the phone, you can go to the Wi-Fi or mobile internet proxy section and set the address to 127.0.0.1 and the port to 8000. You can also set the Telegram proxy to 127.0.0.1, port 9050."
+printf "____________________"
 nmap 127.0.0.1
 tor HTTPtunnelport 8000
