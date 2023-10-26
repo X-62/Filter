7d3bf30a679bf0f7482bb1444ffb84c5c409a415
X-62 <142080539+X-62@users.noreply.github.com>
2023-10-26 12:04:37 +0330

    Create X.sh

---
+++ X.sh
@@ -0,0 +1,3 @@
+echo "To connect the proxy to the phone, you can go to the Wi-Fi or mobile internet proxy section and set the address to 127.0.0.1 and the port to 8000. You can also set the Telegram proxy to 127.0.0.1, port 9050."
+nmap 127.0.0.1
+tor HTTPtunnelport 8000
