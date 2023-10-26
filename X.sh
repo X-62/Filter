09949f4ab425a4b45bac19907009005a70b1379e
X-62 <142080539+X-62@users.noreply.github.com>
2023-10-26 12:44:37 +0330

    Update X.sh

--- X.sh
+++ X.sh
@@ -1,9 +1,9 @@
 clear
 
 printf "____________________"
-
-printf "To connect the proxy to the phone, you can go to the Wi-Fi or mobile internet proxy section and set the address to 127.0.0.1 and the port to 8000. You can also set the Telegram proxy to 127.0.0.1, port 9050."
-
+echo "_"
+echo "To connect the proxy to the phone, you can go to the Wi-Fi or mobile internet proxy section and set the address to 127.0.0.1 and the port to 8000. You can also set the Telegram proxy to 127.0.0.1, port 9050."
+echo "_"
 printf "____________________"
 
 echo "_"
